84c577a2d543db9b92841bb9e83b9c913ea35229
gConstante2 <272401@unifio.edu.br>
2026-09-17 14:04:43 +0000

    exclusão da pasta projeto1 (pasta duplicada com erro)

--- projeto1/src/app/livros/cad-livro/cad-livro.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-cad-livro',
-  standalone: false,
-  templateUrl: './cad-livro.html',
-  styleUrl: './cad-livro.scss',
-})
-export class CadLivro {}
--- projeto1/src/app/livros/listar/listar.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-listar',
-  standalone: false,
-  templateUrl: './listar.html',
-  styleUrl: './listar.scss',
-})
-export class Listar {}
